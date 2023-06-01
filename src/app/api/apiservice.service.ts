8b433c79db72ca6563261b1e2f25c22a4b523d27
soju-s <soju.ett@gmail.com>
2023-06-01 12:47:01 +0530

    Cocktail Recipes

---
+++ src/app/api/apiservice.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiserviceService {
+
+  constructor(private http:HttpClient) { }
+
+
+  allDrinks(){
+    return this.http.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiserviceService } from './api/apiservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'drinksApi';
+
+  cockTails:any=[]
+
+  constructor(private api:ApiserviceService){}
+
+
+  ngOnInit(): void {
+    this.api.allDrinks().subscribe((result:any)=>{
+      console.log(result.drinks);
+      this.cockTails=result.drinks
+      
+    })
+  }
 }
